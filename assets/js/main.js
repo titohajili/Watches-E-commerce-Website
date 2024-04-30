1ce9efd59c9dee35db89da3cda714ced518b0705
titohajili <maqatoto963@gmail.com>
2024-04-30 16:08:01 +0400

    Testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,16 @@ function scrollHeader(){
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+//=============== Testimonial Swiper ==========
+
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+
+    spaceBetween:30,
+    loop: 'true',
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
